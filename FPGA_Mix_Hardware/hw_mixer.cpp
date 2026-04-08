b885aed1e9ffaf3eea2ee22bfd94300697afa5ac
Iris Kaucher <iriskaucher@gmail.com>
2026-04-08 17:23:48 -0400

    Mix with packing streams

--- FPGA_Mix_Hardware/hw_mixer.cpp
+++ FPGA_Mix_Hardware/hw_mixer.cpp
@@ -3,74 +3,50 @@
 #include "hw_mixer.h"
 
 void mixer(
-    hls::stream<audio_stream>& stream_in_0,
-    hls::stream<audio_stream>& stream_in_1,
-    hls::stream<audio_stream>& stream_in_2,
-    hls::stream<audio_stream>& stream_in_3,
-    hls::stream<audio_stream>& stream_in_4,
-    hls::stream<audio_stream>& stream_in_5,
-    hls::stream<audio_stream>& stream_in_6,
-    hls::stream<audio_stream>& stream_in_7,
+    hls::stream<audio_stream>& stream_in,
     ap_uint<8> switches,
-    hls::stream<audio_stream>& mix_out
+    hls::stream<audio_out>& mix_out
 ) {
-    #pragma HLS INTERFACE axis port=stream_in_0
-    #pragma HLS INTERFACE axis port=stream_in_1
-    #pragma HLS INTERFACE axis port=stream_in_2
-    #pragma HLS INTERFACE axis port=stream_in_3
-    #pragma HLS INTERFACE axis port=stream_in_4
-    #pragma HLS INTERFACE axis port=stream_in_5
-    #pragma HLS INTERFACE axis port=stream_in_6
-    #pragma HLS INTERFACE axis port=stream_in_7
+    #pragma HLS INTERFACE axis port=stream_in
     #pragma HLS INTERFACE ap_none port=switches
     #pragma HLS INTERFACE axis port=mix_out
-    #pragma HLS INTERFACE s_axilite port=return
+    #pragma HLS INTERFACE ap_ctrl_none port=return
     #pragma HLS ARRAY_PARTITION variable=recip_sqrt_lut complete
 
-    while(1) {
-    #pragma HLS LOOP_TRIPCOUNT min=200 max=20000 avg=10000
-    // Read all 8 streams
-    audio_stream s0;
-    audio_stream s1;
-    audio_stream s2;
-    audio_stream s3;
-    audio_stream s4;
-    audio_stream s5;
-    audio_stream s6;
-    audio_stream s7;
-    stream_in_0.read(s0);
-    stream_in_1.read(s1);
-    stream_in_2.read(s2);
-    stream_in_3.read(s3);
-    stream_in_4.read(s4);
-    stream_in_5.read(s5);
-    stream_in_6.read(s6);
-    stream_in_7.read(s7);
+    audio_stream sample = stream_in.read();
+    ap_int<16> s0 = sample.data(15,0);
+    ap_int<16> s1 = sample.data(31,16);
+    ap_int<16> s2 = sample.data(47,32);
+    ap_int<16> s3 = sample.data(63,48);
+    ap_int<16> s4 = sample.data(79,64);
+    ap_int<16> s5 = sample.data(95,80);
+    ap_int<16> s6 = sample.data(111,96);
+    ap_int<16> s7 = sample.data(127,112);
+
+
+
 
     // initialize before accumulating
     int mix_int = 0;
     int active = 0;
 
-    if (switches[0]) { mix_int += s0.data.to_int(); active++; }
-    if (switches[1]) { mix_int += s1.data.to_int(); active++; }
-    if (switches[2]) { mix_int += s2.data.to_int(); active++; }
-    if (switches[3]) { mix_int += s3.data.to_int(); active++; }
-    if (switches[4]) { mix_int += s4.data.to_int(); active++; }
-    if (switches[5]) { mix_int += s5.data.to_int(); active++; }
-    if (switches[6]) { mix_int += s6.data.to_int(); active++; }
-    if (switches[7]) { mix_int += s7.data.to_int(); active++; }
+    if (switches[0]) { mix_int += s0; active++; }
+    if (switches[1]) { mix_int += s1; active++; }
+    if (switches[2]) { mix_int += s2; active++; }
+    if (switches[3]) { mix_int += s3; active++; }
+    if (switches[4]) { mix_int += s4; active++; }
+    if (switches[5]) { mix_int += s5; active++; }
+    if (switches[6]) { mix_int += s6; active++; }
+    if (switches[7]) { mix_int += s7; active++; }
 
     // Build output packet and write to stream
-    audio_stream out_pkt;
-    out_pkt.data = (active > 0) ? (int64_t)mix_int * recip_sqrt_lut[active] >> 8 : 0;
-    out_pkt.keep = -1;
-    out_pkt.strb = -1;
-    out_pkt.last = 0;
-    if (s0.last == 1 || s1.last == 1 || s2.last == 1 || s3.last == 1 || s4.last == 1 || s5.last == 1 || s6.last == 1 || s7.last == 1) {
-            out_pkt.last = 1;
-            mix_out.write(out_pkt);
-            break;
-        }
-    mix_out.write(out_pkt);
+    audio_out out_pkt;
+    if(active > 0)
+    {
+        mix_int = (mix_int * (int)recip_sqrt_lut[active]) >> 8;
     }
+    out_pkt.data(31, 16) = (ap_int<16>)mix_int;
+    out_pkt.data(15, 0) = 0;
+    out_pkt.last = sample.last;
+    mix_out.write(out_pkt);
 }
--- FPGA_Mix_Hardware/hw_mixer.h
+++ FPGA_Mix_Hardware/hw_mixer.h
@@ -3,22 +3,16 @@
 
 #include "ap_axi_sdata.h"
 
-typedef ap_axiu<32, 2, 5, 6> audio_stream;
+typedef ap_axis<128, 1, 1, 1> audio_stream;
+typedef ap_axis<32, 1, 1, 1> audio_out;
 
 // 256/sqrt(0-8) this is used so we can later use multiplication intead of division and shift the bits by 8
-static const int recip_sqrt_lut[9] = {
+static const short recip_sqrt_lut[9] = {
     0, 256, 181, 148, 128, 114, 105, 97, 91
 };
 
 void mixer(
-    hls::stream<audio_stream>& stream_in_0,
-    hls::stream<audio_stream>& stream_in_1,
-    hls::stream<audio_stream>& stream_in_2,
-    hls::stream<audio_stream>& stream_in_3,
-    hls::stream<audio_stream>& stream_in_4,
-    hls::stream<audio_stream>& stream_in_5,
-    hls::stream<audio_stream>& stream_in_6,
-    hls::stream<audio_stream>& stream_in_7,
+    hls::stream<audio_stream>& stream_in,
     ap_uint<8> switches,
     hls::stream<audio_stream>& mix_out
 );
